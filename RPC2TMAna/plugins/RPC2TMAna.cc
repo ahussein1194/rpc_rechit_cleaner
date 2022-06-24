97ef43843dbd88d4a1de76eb085fc7812bf1bca4
Ahmed Hussein <ahmed.hussein@cern.ch>
2022-06-24 16:59:19 +0200

    edit

--- RPC2TMAna/plugins/RPC2TMAna.cc
+++ RPC2TMAna/plugins/RPC2TMAna.cc
@@ -176,9 +176,9 @@ void RPC2TMAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 // Taken from RPCHitCleaner.cc
 
-RPCDigiCollection m_inrpcDigis = digiCollectionRPCTwinMux;
-//typedef  DigiContainerIterator<RPCDetId, RPCDigi> DigiRangeIterator;
-//DigiRangeIterator m_inrpcDigis = digiCollectionRPCTwinMux;
+//RPCDigiCollection m_inrpcDigis = digiCollectionRPCTwinMux;
+typedef  DigiContainerIterator<RPCDetId, RPCDigi> DigiRangeIterator;
+DigiRangeIterator m_inrpcDigis = digiCollectionRPCTwinMux;
 //std::cout << *digiCollectionRPCTwinMux.first << std::endl;
 for(hit = m_inrpcDigis->begin(); hit != m_inrpcDigis->end(); ++hit) {
   RPCDetId rpcDetId = (*hit).first;
