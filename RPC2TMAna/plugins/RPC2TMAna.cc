135091fd55fea2a6506f09ebede0b971dc6a96c6
Ahmed Hussein <ahmed.hussein@cern.ch>
2022-06-25 15:30:03 +0200

    edit

--- RPC2TMAna/plugins/RPC2TMAna.cc
+++ RPC2TMAna/plugins/RPC2TMAna.cc
@@ -180,10 +180,10 @@ void RPC2TMAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 typedef  DigiContainerIterator<RPCDetId, RPCDigi> DigiRangeIterator;
 edm::Handle<RPCDigiCollection> m_inrpcDigis = digiCollectionRPCTwinMux;
 //std::cout << *m_inrpcDigis << std::endl;
-/*for(auto hit = m_inrpcDigis->begin(); hit != m_inrpcDigis->end(); ++hit) {
+for(auto hit = m_inrpcDigis->begin(); hit != m_inrpcDigis->end(); ++hit) {
   RPCDetId rpcDetId = (*hit).first;
   std::cout << "Region is: " << rpcDetId.region() << std::endl;
-}*/
+}
 
 
 #ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
