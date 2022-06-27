74e009d9e5a37b987271170467ac459a5d3b02ff
Ahmed Hussein <ahmed.hussein@cern.ch>
2022-06-28 01:26:39 +0200

    edit

--- RPC2TMAna/plugins/RPC2TMAna.cc
+++ RPC2TMAna/plugins/RPC2TMAna.cc
@@ -272,7 +272,7 @@ for(auto chamber = m_inrpcDigis->begin(); chamber != m_inrpcDigis->end(); ++cham
       cluster_id++; // assign a new index for the next cluster.
     }
     itr++;
-    cluster_size += 1;
+    cluster_size++;
 
     // Hits belong to cluster with cluster_id.
     // Store the info of the hit in a tmp variable.
